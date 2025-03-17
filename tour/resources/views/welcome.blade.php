c5ad2d11d5626cb0a7504fe05f1044afb496abb7
Dev Selimcan <selimcangursu@asemsoft.com>
2025-03-17 23:05:31 +0300

    anasayfa hero reklam alanı tasarlandı responsive hale getirildi.

--- tour/resources/views/welcome.blade.php
+++ tour/resources/views/welcome.blade.php
@@ -15,6 +15,30 @@
             </select>
             <button>Ara</button>
         </div>
+     </div>
+</section>
+<section class="hero__advert__card">
+    <div class="advert__card">
+        <img src="https://cdn-icons-png.freepik.com/256/10913/10913730.png?uid=R190373578&ga=GA1.1.655176800.1740923195&semt=ais_hybrid" alt="">
+        <div class="advert__card__main">
+            <h4>Hızlı ve Güvenilir</h4>
+            <small>Hizmetlerimizle her zaman yanınızdayız.</small>
+        </div>
+    </div>
+    <div class="advert__card">
+        <img src="https://cdn-icons-png.freepik.com/256/14121/14121701.png?uid=R190373578&ga=GA1.1.655176800.1740923195&semt=ais_hybrid" alt="">
+        <div class="advert__card__main">
+            <h4>7/24 Destek</h4>
+            <small>Her an, her yerde bizimle iletişime geçin.</small>
+        </div>
+    </div>
+    <div class="advert__card">
+        <img src="https://cdn-icons-png.freepik.com/256/15836/15836890.png?uid=R190373578&ga=GA1.1.655176800.1740923195&semt=ais_hybrid" alt="">
+        <div class="advert__card__main">
+            <h4>Memnuniyet Garantisi</h4>
+            <small>Müşteri memnuniyeti bizim önceliğimizdir.</small>
+        </div>
     </div>
 </section>
+
 @endsection
